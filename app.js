403741e8d45fb1d0910751740dae241bfb1ece06
Zorochan404 <dram8898@gmail.com>
2025-06-27 13:21:21 +0530

    added cors for test website

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://admin.inframeschool.com', 'https://test3-iota-ten.vercel.app, https://inframe-wine.vercel.app'],
+    origin: ['http://localhost:3000', 'https://admin.inframeschool.com', 'https://test3-iota-ten.vercel.app', 'https://inframe-wine.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
